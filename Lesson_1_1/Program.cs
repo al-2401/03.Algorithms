709f4ebca4d2173fa5e56c7dca72346a70f3a327
al-2401 <mail.al2401@gmail.com>
2021-04-16 10:51:56 +0300

    Lesson_1_1

--- Lesson_1_1/Program.cs
+++ Lesson_1_1/Program.cs
@@ -6,7 +6,29 @@ namespace Lesson_1_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var n = 6;
+            if (CheckPrimeNumber(n))
+                Console.WriteLine($"Число {n} простое");
+            else
+                Console.WriteLine($"Число {n} не является простым");
+        }
+
+        static bool CheckPrimeNumber(int n)
+        {
+            var d = 0;
+            for (var i = 2; i < n; i++)
+            {
+                if (n % i == 0)
+                    d++;
+            }
+            switch (d)
+            {
+                case 0:
+                    return true;
+
+                default:
+                    return false;
+            }
         }
     }
 }
