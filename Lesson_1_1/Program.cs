f6ce38bd7e17dfb2d5186db8cf407335bce5231e
al-2401 <mail.al2401@gmail.com>
2021-04-16 18:34:49 +0300

    функция вычисления числа Фибоначчи

--- Lesson_1_1/Program.cs
+++ Lesson_1_1/Program.cs
@@ -6,6 +6,7 @@ namespace Lesson_1_1
     {
         public class TestCase
         {
+            public int Id { get; set; }
             public int N { get; set; }
             public bool Expected { get; set; }
             public Exception ExpectedException { get; set; }
@@ -19,11 +20,11 @@ namespace Lesson_1_1
 
                 if (actual == testCase.Expected)
                 {
-                    Console.WriteLine("VALID TEST");
+                    Console.WriteLine($"Тест № {testCase.Id} - VALID TEST");
                 }
                 else
                 {
-                    Console.WriteLine("INVALID TEST");
+                    Console.WriteLine($"Тест № {testCase.Id} - INVALID TEST Expected {testCase.Expected}");
                 }
             }
             catch (Exception ex)
@@ -34,11 +35,11 @@ namespace Lesson_1_1
                     //какой тип исключения указать в функции CheckPrimeNumber
                     //чтобы обработать в этом тесте
                     //TODO add type exception tests;
-                    Console.WriteLine("VALID TEST");
+                    Console.WriteLine($"Тест № {testCase.Id} - VALID TEST");
                 }
                 else
                 {
-                    Console.WriteLine("INVALID TEST");
+                    Console.WriteLine($"Тест № {testCase.Id} - INVALID TEST");
                 }
             }
         }
@@ -49,6 +50,7 @@ namespace Lesson_1_1
             {
                 //n = n > 0 ? n : n * -1;
                 n = Math.Abs(n);
+                if (n == 1) return false;
                 var d = 0;
                 for (var i = 2; i < n; i++)
                 {
@@ -66,31 +68,52 @@ namespace Lesson_1_1
             }
             catch
             {
-                throw new ArgumentException("\"n\" не является целым числом");
+                throw new ArgumentException("\"n\" ошибка при нахождении простого числа");
             }
         }
         static void Main(string[] args)
         {
-            var n = 6;
+            var n = 4;
             if (CheckPrimeNumber(n))
                 Console.WriteLine($"Число {n} простое");
             else
                 Console.WriteLine($"Число {n} не является простым\n");
 
             var testCase1 = new TestCase();
-            testCase1.N = 5;
-            testCase1.Expected = true;
+            testCase1.Id = 1;
+            testCase1.N = 1;
+            testCase1.Expected = false;
             testCase1.ExpectedException = null;
 
             var testCase2 = new TestCase();
-            testCase2.N = 6;
-            testCase2.Expected = false;
+            testCase2.Id = 2;
+            testCase2.N = 2;
+            testCase2.Expected = true;
             testCase2.ExpectedException = null;
+
+            var testCase3 = new TestCase();
+            testCase3.Id = 3;
+            testCase3.N = 4;
+            testCase3.Expected = false;
+            testCase3.ExpectedException = null;
+
+            var testCase4 = new TestCase();
+            testCase4.Id = 4;
+            testCase4.N = -5;
+            testCase4.Expected = true;
+            testCase4.ExpectedException = null;
+
+            var testCase5 = new TestCase();
+            testCase5.Id = 5;
+            testCase5.N = -6;
+            testCase5.Expected = false;
+            testCase5.ExpectedException = null;
 
             TestCheckPrimeNumber(testCase1);
             TestCheckPrimeNumber(testCase2);
-
-
+            TestCheckPrimeNumber(testCase3);
+            TestCheckPrimeNumber(testCase4);
+            TestCheckPrimeNumber(testCase5);
         }
     }
 }
---
+++ Lesson_1_3/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Lesson_1_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Числа Фибоначчи то 0 до 1000 методом цикла");
+            GetFibonacciNumbersFor(1000);
+            Console.WriteLine("\nЧисла Фибоначчи то 0 до 1000 методом рекурсии");
+            GetFibonacciNumbersRec(1000);
+        }
+
+        /// <summary>
+        /// Числа Фибоначчи то 0 до n методом цикла
+        /// </summary>
+        static void GetFibonacciNumbersFor(int n)
+        {
+            var prev1 = 1;
+            var prev2 = 0;
+            var fib = 0;
+            while (true)
+            {
+                fib = prev1 + prev2;
+                prev2 = prev1;
+                prev1 = fib;
+                if (fib <= n)
+                    Console.WriteLine(fib);
+                else
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Числа Фибоначчи то 0 до n методом рекурсии
+        /// </summary>
+        static void GetFibonacciNumbersRec(int n, int fib = 0, int prev1 = 1, int prev2 = 0)
+        {
+            fib = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = fib;
+            if (fib <= n)
+            {
+                Console.WriteLine(fib);
+                GetFibonacciNumbersRec(n, fib, prev1, prev2);
+            }
+        }
+    }
+}
